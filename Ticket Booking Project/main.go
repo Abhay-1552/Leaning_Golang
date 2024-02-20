927c8a33bb28f1702e108289f3550b3e40425c85
Abhay-1552 <patelabhay1552@gmail.com>
2024-02-20 17:49:37 +0530

    Learning

--- Ticket Booking Project/main.go
+++ Ticket Booking Project/main.go
@@ -5,50 +5,25 @@ import (
 	"strings"
 )
 
+const conferenceTicket = 50
+
+var conferenceName = "Go Conference"
+var remainingTickets uint = 50 // uint is an unsigned integer
+var bookings []string          // Array of strings of undefined length
+
 func main() {
-
-	// Use of variables
-	var conferenceName = "Go Conference"
-	const conferenceTicket = 50
-	var remainingTickets uint = 50 // uint is an unsigned integer
-	var bookings []string          // Array of strings of undefined length
-
-	fmt.Println("Welcome to the", conferenceName, "booking system.")
-	fmt.Println("We have", remainingTickets, "tickets remaining for the", conferenceName, "out of", conferenceTicket, "tickets.")
-
-	// Use to repeat a string multiple times
-	fmt.Println(strings.Repeat("-", 75))
+	// function to greet the user
+	greetUser()
 
 	for {
-		// Declare variables
-		var firstName string
-		var lastName string
-		var email string
-		var userTicket uint
+		// function to get user input
+		var firstName, lastName, email, userTicket = getUserInput()
 
-		// Ask user for their name and the number of tickets they want to book
-		fmt.Print("\nEnter your First Name: ")
-		fmt.Scan(&firstName)
-
-		fmt.Print("Enter your Last Name: ")
-		fmt.Scan(&lastName)
-
-		fmt.Print("Enter your Email: ")
-		fmt.Scan(&email)
-
-		fmt.Print("Enter the number of tickets you want to book: ")
-		fmt.Scan(&userTicket)
-
-		// Validate the user's input
-		var validName = len(firstName) > 2 && len(lastName) > 2
-
-		// Use to check if a string contains a substring
-		var validEmail = strings.Contains(email, "@") && strings.Contains(email, ".")
-
-		var validTicket = userTicket > 0 && userTicket <= remainingTickets
+		// function to validate user input
+		var isValidName, isValidEmail, isValidTicket = validateUser(firstName, lastName, email, userTicket, remainingTickets)
 
 		// If the user data is valid, book the tickets
-		if validName && validEmail && validTicket {
+		if isValidName && isValidEmail && isValidTicket {
 			fmt.Println("\nThank you", firstName, lastName, "for booking", userTicket, "tickets for the", conferenceName)
 
 			// reduce the number of tickets
@@ -60,26 +35,17 @@ func main() {
 			var fullName = firstName + " " + lastName
 			bookings = append(bookings, fullName)
 
-			firstNames := []string{}
-			// _ is a blank identifier used to ignore the index
-			for _, name := range bookings {
-
-				// Use to split a string into substrings
-				var first = strings.Fields(name)
-
-				firstNames = append(firstNames, first[0])
-			}
-
+			var firstNames = printFirstName(bookings, conferenceName)
 			fmt.Println("\nThe following people have booked tickets for the", conferenceName, ":", firstNames)
 
 		} else {
-			if !validName {
+			if !isValidName {
 				fmt.Println("\nSorry, your name is invalid. Please try again.")
 			}
-			if !validEmail {
+			if !isValidEmail {
 				fmt.Println("\nSorry, your email is invalid. Please try again.")
 			}
-			if !validTicket {
+			if !isValidTicket {
 				fmt.Println("\nSorry, the number of tickets you want to book is invalid. Please try again.")
 			}
 			continue
@@ -91,4 +57,62 @@ func main() {
 			break
 		}
 	}
+}
+
+// Greet the user
+func greetUser() {
+	fmt.Println("Welcome to the", conferenceName, "booking system.")
+	fmt.Println("We have", remainingTickets, "tickets remaining for the", conferenceName, "out of", conferenceTicket, "tickets.")
+
+	// Use to repeat a string multiple times
+	fmt.Println(strings.Repeat("-", 75))
+}
+
+// Get the user's input
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTicket uint
+
+	fmt.Print("\nEnter your First Name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter your Last Name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your Email: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter the number of tickets you want to book: ")
+	fmt.Scan(&userTicket)
+
+	return firstName, lastName, email, userTicket
+}
+
+// Validate the user's input
+func validateUser(firstName string, lastName string, email string, userTicket uint, remainingTickets uint) (bool, bool, bool) {
+	var isValidName = len(firstName) > 2 && len(lastName) > 2
+
+	// Use to check if a string contains a substring
+	var isValidEmail = strings.Contains(email, "@") && strings.Contains(email, ".")
+
+	var isValidTicket = userTicket > 0 && userTicket <= remainingTickets
+
+	return isValidName, isValidEmail, isValidTicket
+}
+
+// Print the first name of the people who have booked tickets
+func printFirstName(bookings []string, conferenceName string) []string {
+	firstNames := []string{}
+
+	// _ is a blank identifier used to ignore the index
+	for _, name := range bookings {
+
+		// Use to split a string into substrings
+		var first = strings.Fields(name)
+
+		firstNames = append(firstNames, first[0])
+	}
+	return firstNames
 }
